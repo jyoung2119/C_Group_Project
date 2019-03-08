5356526e0609404ec7cc939559b237a03d78cb9c
LinearSoop <34359129+Onawa33@users.noreply.github.com>
2019-03-08 13:03:09 -0600

    Add files via upload

---
+++ crustFunction.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int crustFunc();
+
+int main()
+{
+
+    int crustFunc()
+    {
+        int choice = 0;
+        char crustyString[3] [8] = {"Thicc", "Mediocre", "Thin";
+
+        printf("Which crust would you like? Choose\n 1- Thicc\n 2- Mediocre\n 3- Thin\n");
+        scanf("%d", &choice);
+
+        case 1:
+            crustyString = "Thicc";
+            break;
+
+
+        printf("Your crust choice is: %s", crustyString);
+
+        return()
+    }
+
+}
+
