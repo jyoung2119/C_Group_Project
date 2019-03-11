8f5591f1bb11c0bf59fde72a2c9c2ddf7ae11714
LinearSoop <34359129+Onawa33@users.noreply.github.com>
2019-03-11 11:02:24 -0500

    CrustyFunc Complete?

--- crustFunction.h
+++ crustFunction.h
@@ -1,27 +1,56 @@
-#include <stdio.h>
+//this is a .h file to be included in the main pizza menu. 
+//it returns a string which is the choice of crust by user.
 
-int crustFunc();
+#include <stdio.h>
+#include <string.h>
+
+char crustFunc();
 
 int main()
 {
-
-    int crustFunc()
+    crustFunc();
+}
+char crustFunc()
     {
         int choice = 0;
-        char crustyString[3] [8] = {"Thicc", "Mediocre", "Thin";
+        char * crustyString;
 
-        printf("Which crust would you like? Choose\n 1- Thicc\n 2- Mediocre\n 3- Thin\n");
+        printf("Which crust would you like? Choose\n 1- Thick\n 2- Hand-tossed\n 3- Thin\n 4- Cheese-stuffed\n 5- Deep-dish\n");
         scanf("%d", &choice);
 
-        case 1:
-            crustyString = "Thicc";
-            break;
+        if(choice < 1 || choice > 5)
+        {
+            printf("ERROR: Enter a number from 1-5\n");
+            crustFunc();
+        }
+        else 
+        {
+            switch(choice)
+            {
+            case 1:
+                crustyString = "Thick";
+                break;
 
+            case 2: 
+                crustyString= "Hand-tossed";
+                break;
+            
+            case 3: 
+                crustyString = "Thin";
+                break;
+            
+            case 4:
+                crustyString = "Cheese-stuffed";
+                break;
+            
+            case 5:
+                crustyString = "Deep Dish";
+                break;
+            }
+            //printf("Your crust choice is: %s\n", crustyString);    
+        }           
 
-        printf("Your crust choice is: %s", crustyString);
-
-        return()
+        return *crustyString;
     }
 
-}
 
